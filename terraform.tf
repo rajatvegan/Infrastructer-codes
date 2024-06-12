2909ee682391b357f46328645e177e3f1e7b28b8
rajatvegan <rajatkatiyar4@gmail.com>
2024-06-12 23:43:23 +0530

    added terraform.tf

---
+++ terraform.tf
@@ -0,0 +1,16 @@
+terraform {
+  required_providers {
+
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.0.2"
+    }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+
+
+  }
+}
